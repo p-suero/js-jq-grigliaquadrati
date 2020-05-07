36ef08141f6d3bb1990dc35296fbaa30b02a9998
Paolo <paolo.suero@gmail.com>
2020-05-07 18:44:44 +0200

    Aggiunta seconda opzione nel file JS

--- main.js
+++ main.js
@@ -4,31 +4,46 @@
 // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
 
 
-//creo con j query 25 quadratini
+//OPZIONE A senza l'utilizzo di each (in questo modo scrivo poco codice)
+//inserisco 25 quadratini con j jQuery
 for (var i = 0; i < 25; i++) {
-    $("#griglia").append("<div class='quadrato'><p></p></div>")
+    var random = getRndInteger(0, 10);
+    //se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+    if (random == 0) {
+        $("#griglia").append("<div class='quadrato green'><p>" + random + "</p></div>");
+    } else if (random % 2 == 0) {
+        $("#griglia").append("<div class='quadrato red'><p>" + random + "</p></div>");
+    } else {
+        $("#griglia").append("<div class='quadrato'><p>" + random + "</p></div>");
+    }
 }
 
-//seleziono tutti i quadratini e li esamino uno per uno con la funzione each
-$(".quadrato p").each(function() {
-    //genero un numero random tra 0 e 10 con la funzione
-    var random = getRndInteger(0, 10);
-    console.log(random);
-    //creo la condizione per aggiungere i numeri nei quadrati secondo il colore
-    if(random == 0) {
-        $(this).append(random);
-        $(this).addClass("green");
-    } else if (random % 2 == 0) {
-        $(this).append(random);
-        $(this).addClass("red");
-    } else {
-        $(this).append(random);
-    }
-})
+
+// //OPZIONE B con l'utilizzo di each (si fa qualche passaggio in più)
+// //creo con j query 25 quadratini
+// for (var i = 0; i < 25; i++) {
+//     $("#griglia").append("<div class='quadrato'><p></p></div>")
+// }
+//
+// //seleziono tutti i quadratini e li esamino uno per uno con la funzione each
+// $(".quadrato p").each(function() {
+//     //genero un numero random tra 0 e 10 con la funzione sotto creata
+//     var random = getRndInteger(0, 10);
+//     console.log(random);
+//     //creo la condizione per aggiungere i numeri nei quadrati secondo il colore
+//     if(random == 0) {
+//         $(this).append(random);
+//         $(this).addClass("green");
+//     } else if (random % 2 == 0) {
+//         $(this).append(random);
+//         $(this).addClass("red");
+//     } else {
+//         $(this).append(random);
+//     }
+// })
 
 
-
-//creo una funzione che mi restituisce un array con numeri casuali
+//creo una funzione che generi un numero casuale
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
